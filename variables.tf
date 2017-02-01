49959efa7cedc2e76d2b071135dff06494925bc2
kukkut <reshma.mallya@capgemini.com>
2017-02-01 16:07:25 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+        default = "ap-northeast-1"
+}
+variable "availability_zone" {
+        default = "ap-northeast-1a"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "aws_ami" {
+        default = "ami-5dd8b73a"
+}
+
+variable "key_name" {
+        default = "resh95"
+}
+
+variable "aws_access_key" {
+        default = "AKIAJZN4L4JOCHOHQ32A"
+}
+
+variable "aws_secret_key" {
+        default = "SbHwuqy8oc9j0apNPdaxucRWnHBGdTPjJz+rBBo6"
+}
+
